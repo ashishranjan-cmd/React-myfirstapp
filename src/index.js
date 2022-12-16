a35cab4db4990feb7624853559f4b05f87987269
Ashish Ranjan <akp9801@gmail.com>
2022-12-16 22:15:40 +0530

    created dynamic element jsx code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Welcome to Dynamic Island"
+// document.getElementById("d1").appendChild(h1); /* this will create second child in div 1 */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// declarative  language - what to do , where to do
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const React =  require("react") // to fetch react module
+const ReactDOM = require("react-dom") // to get react dom
+
+// react use complier behind the scene for frontend js : babel
+// babel convert the code into browser compatible javascript
+
+// JSX code
+
+ReactDOM.render(<h1>Welcome to my webpage</h1>, document.getElementById("d1"))
